e81dbba37e3fee94222d3cb754a53eba6eb317d3
Zunaira Rasheed <rasheedzunaira119@gmail.com>
2026-04-10 13:51:27 +0500

    program to callig roll number

---
+++ hello_world/nameif.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+int girl1 = 1; 
+int girl2 = 2;
+int boy1 = 3;
+int boy2 = 4;
+int x;
+x = 2;
+
+if(x == girl1) {
+    printf("%d is girl1 roll number.\n", x);
+ } 
+if(x == boy1) {
+    printf("%d is boy1 roll number.\n", x);
+}
+if(x == girl2) {
+    printf("%d is girl2 roll number.\n", x);
+}
+   if(x == boy2) {
+    printf("%d is boy2 roll number.\n", x);
+} 
+    return 0;
+}
