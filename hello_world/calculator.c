f82549f98abf0b0303287ab6febaabd86172c344
Zunaira Rasheed <rasheedzunaira119@gmail.com>
2026-07-14 16:31:32 +0500

    code for making calculator

--- hello_world/calculator.c
+++ hello_world/calculator.c
@@ -23,16 +23,22 @@ float divide(float a, float b) {
 int main() {
     float num1, num2, result;
     int choice;
+for (int i =1;i<=100;i++) {
 
-    printf("Welcome to Calculator\n");
+    printf("\n=====Calculator-Run#%d=====\n",i);
     printf("Select Operation:\n");
     printf("1. Add\n");
     printf("2. Subtract\n");
     printf("3. Multiply\n");
     printf("4. Divide\n");
-    
-    printf("Enter choice 1/2/3/4: ");
+    printf("5. Exit\n");
+
+    printf("Enter choice 1/2/3/4/5: ");
     scanf("%d", &choice);
+    if (choice == 5) {
+        printf("Thanks for using the calculator!\n");
+        break;
+    }
 
     printf("Enter first number: ");
     scanf("%f", &num1);
@@ -42,25 +48,26 @@ int main() {
 
     if (choice == 1) {
         result = add(num1, num2);
-        printf("%.2f + %.2f = %.2f\n", num1, num2, result);
+        printf("%f + %f = %f\n", num1, num2, result);
     }
     else if (choice == 2) {
         result = subtract(num1, num2);
-        printf("%.2f - %.2f = %.2f\n", num1, num2, result);
+        printf("%f - %f = %f\n", num1, num2, result);
     }
     else if (choice == 3) {
         result = multiply(num1, num2);
-        printf("%.2f * %.2f = %.2f\n", num1, num2, result);
+        printf("%f * %f = %f\n", num1, num2, result);
     }
     else if (choice == 4) {
         if(num2 != 0) {
             result = divide(num1, num2);
-            printf("%.2f / %.2f = %.2f\n", num1, num2, result);
+            printf("%f / %f = %f\n", num1, num2, result);
         }
     }
     else {
         printf("Invalid choice\n");
     }
+}
 
     return 0;
 }
