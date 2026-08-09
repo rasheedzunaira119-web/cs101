7a9ea3d2adbd69b6a583bb46f5a0ed3d18112fc4
Zunaira Rasheed <rasheedzunaira119@gmail.com>
2026-08-09 17:22:29 +0500

    program for not operation

---
+++ hello_world/operationnot.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool isLoggedIn = false;
+
+    if (!isLoggedIn) {  
+        cout << "Pehle login karo";
+    }else {
+        cout << "Welcome back!";
+    }
+    return 0;
+}
